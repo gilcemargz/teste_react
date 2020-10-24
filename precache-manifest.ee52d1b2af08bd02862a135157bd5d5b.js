8e58b44f64fc8f7e44e2cbbd1521c5566aece327
gilcemargz <gilcemargz@hotmail.com>
2020-10-24 10:29:07 -0300

    Updates

---
+++ precache-manifest.ee52d1b2af08bd02862a135157bd5d5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0489049020a79352fb8da6410afb18",
+    "url": "/teste_react/jogo_da_velha/index.html"
+  },
+  {
+    "revision": "a64eb1371ef8bb141d09",
+    "url": "/teste_react/jogo_da_velha/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "a386015cdc65bc8873d0",
+    "url": "/teste_react/jogo_da_velha/static/js/2.e5fe2c6e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/teste_react/jogo_da_velha/static/js/2.e5fe2c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64eb1371ef8bb141d09",
+    "url": "/teste_react/jogo_da_velha/static/js/main.1faac050.chunk.js"
+  },
+  {
+    "revision": "c4a0e50e3e39f197c931",
+    "url": "/teste_react/jogo_da_velha/static/js/runtime-main.92b56d94.js"
+  }
+]);
