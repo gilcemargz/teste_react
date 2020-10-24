2fc91234c5e6a38ca29d0c138a06c7d7f450438b
gilcemargz <gilcemargz@hotmail.com>
2020-10-24 14:28:52 -0300

    k

--- pagina_aula/src/index.js
+++ pagina_aula/src/index.js
@@ -4,14 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = 'Gilcemar'
+var element = <h1>Hello, {name}</h1>
+
+const user = {
+  firstName: 'Gilcemar',
+  lastName: 'Guizzo Zanette'
+}
+
+function formatName(parametros) {
+  return parametros.firstName + ' ' + parametros.lastName
+}
+
+element = <h1> Hello, {formatName(user)}</h1>
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
